6e3dc118173613aaa5e1d0363ad6603d55f5f7fb
elmahone <mahonen93@gmail.com>
2016-04-05 13:35:51 +0300

    konsoliin tulostuva valmis

--- app.js
+++ app.js
@@ -1,81 +1,44 @@
-var numOfPlayers = 4;
-var scale = 50;
-var grid = 5;
+var scale = [];
 
-var cardArray = [];
-var cardArrayLength = grid * grid;
-var player = [];
-
-//function shuffle(array) {
-//    var currentIndex = array.length,
-//        temporaryValue, randomIndex;
-//
-//    // While there remain elements to shuffle...
-//    while (0 !== currentIndex) {
-//
-//        // Pick a remaining element...
-//        randomIndex = Math.floor(Math.random() * currentIndex);
-//        currentIndex -= 1;
-//
-//        // And swap it with the current element.
-//        temporaryValue = array[currentIndex];
-//        array[currentIndex] = array[randomIndex];
-//        array[randomIndex] = temporaryValue;
-//    }
-//
-//    return array;
-//}
-/*
-for (var u = 0; u < numOfPlayers; u++) {
-    console.log(u);
-    var cardArray = [];
-    for (var i = 1; i <= numbers; i++) {
-        numArray.push(i);
+function generateNumArray(numberRange) {
+    for (var i = 1; i <= numberRange; i++) {
+        scale.push(i);
     }
-    shuffle(numArray);
-    
-    for (var o = 1; o <= cardArrayLength; o++) {
-        cardArray.push(numArray[o]);
-        numArray.splice(o, 1);
-
-    }
-    player.push(cardArray);
-    console.log(cardArray);
+    return scale;
 }
 
-console.log(player);
-console.log(numArray);
-*/
 
-
-var numArray = [];
-var bingoRow = [];
-function generateNumArray(scale) {
-    for (var i = 1; i <= scale; i++) {
-        numArray.push(i);
-    }
-    return numArray;
-};
-generateNumArray(75);
-console.log(numArray);
-
-function generateBingoRow(rowLength, numberRange) {
+function generateBingoRow(rowLength, numberArr) {
     
+    var bingoRow = [];
     for (var i = 1; i <= rowLength; i++) {
-        var randomNumber = Math.floor((Math.random() * numberRange.length));
-        bingoRow.push(numberRange[randomNumber]);
-        numArray.splice(randomNumber,1)
+        var randomNumber = Math.floor((Math.random() * numberArr.length));
+        bingoRow.push(numberArr[randomNumber]);
+        numberArr.splice(randomNumber, 1);
     }
     return bingoRow;
+}
 
-};
-
-
-generateBingoRow(5, numArray);
-console.log(bingoRow);
-console.log(numArray);
-function generateBingoGrid(rows) {
-    for(var i = 1; i <= rows; i++){
-        generateBingoRow(rows, );
+function generateBingoGrid(rows, numberRange) {
+    var cardArray = [];
+    var tempArray = scale.slice();
+    for (var i = 1; i <= rows; i++) {
+        console.log(generateBingoRow(rows, tempArray));
+        cardArray.push(generateBingoRow(rows, tempArray));
     }
-};
+    
+    return cardArray;
+}
+
+function generateBingoGrids(numOfPlayers, rows, numberRange) {
+    generateNumArray(numberRange);
+    var playerArray = [];
+    for (var i = 1; i <= numOfPlayers; i++) {
+        console.log("------"+"Player "+i+"-------");
+        playerArray.push(generateBingoGrid(rows, numberRange));
+    }
+    return playerArray;
+}
+
+
+generateBingoGrids(4, 5, 75);
